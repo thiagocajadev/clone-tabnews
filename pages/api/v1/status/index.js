ffef5819d8613ad7ca9c2ec21b378f898f0bf998
Thiago Cajaíba <51033018+thiagokj@users.noreply.github.com>
2025-05-30 20:07:18 +0000

    refatorando endpoint e desafio de testes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS Sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const databaseInfo = await getDatabaseInfo();
+
   response.status(200).json({
-    chave: "o status está ok!",
+    updated_at: updatedAt,
+    database: {
+      version: databaseInfo.server_version,
+      max_connections: databaseInfo.max_connections,
+      used_connections: databaseInfo.used_connections,
+    },
   });
+}
+
+async function getDatabaseInfo() {
+  const result = await database.query(
+    `SELECT 
+      current_setting('server_version') AS server_version,
+      current_setting('max_connections') AS max_connections,
+      (SELECT COUNT(*) FROM pg_stat_activity) AS used_connections;`,
+  );
+  const resultOneRow = result.rows[0];
+  return resultOneRow;
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // 1. Versão do Postgres
+  const databaseVersion = "16.0";
+  expect(responseBody.database.version).toBe(databaseVersion);
+
+  // 2. Conexões Máximas
+  const databaseMaxConnections = "100";
+  expect(responseBody.database.max_connections).toBe(databaseMaxConnections);
+
+  // 3. Conexões Usadas
+  const databaseUsedConnections = "7";
+  expect(responseBody.database.used_connections).toBe(databaseUsedConnections);
 });
