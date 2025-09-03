32bf44a9f0dde547c9026b63488a7c2db87ee8cd
Thiago Cajaiba <thiago.cajaiba@gmail.com>
2025-09-03 11:09:32 -0300

    feat: add `email.js` infra module

--- infra/email.js
+++ infra/email.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
   port: process.env.EMAIL_SMTP_PORT,
   auth: {
     user: process.env.EMAIL_SMTP_USER,
-    pass: process.env.EMAIL_SMTP_PASS,
+    pass: process.env.EMAIL_SMTP_PASSWORD,
   },
   secure: process.env.NODE_ENV === "production" ? true : false,
 });
